7823b1b8c993830abfe6f15e12992efe2d0c551d
Doug Wolf <douglaswolf@yahoo.com>
2022-03-28 19:46:12 -0700

    See History.h for changes

--- src/button.sv
+++ src/button.sv
@@ -19,8 +19,13 @@ module button#(parameter ACTIVE=1) (input clk, input pin, output q);
     // If ACTIVE=1, an active edge is low-to-high.  If ACTIVE=0, an active edge is high-to-low
     localparam ACTIVE_EDGE = ACTIVE ? 2'b01 : 2'b10;
     
+    // All three bits of button_sync start out in the "inactive" state
     logic [2:0] button_sync = ACTIVE ? 3'b000 : 3'b111;
+    
+    // This count will clock down as a debounce timer
     logic [31:0] debounce_clock = 0;
+    
+    // This will be 1 on any clock cycle that a fully debounced active-going edge is detected
     logic edge_detected = 0;    
     
     // We're going to check for edges on every clock cycle
@@ -34,7 +39,7 @@ module button#(parameter ACTIVE=1) (input clk, input pin, output q);
         // Presume for the moment that we haven't detected the active-going edge of the button        
         edge_detected <= 0;       
         
-        // If the debounce clock is about to expire, find out of the user-specicied pin is still high
+        // If the debounce clock is about to expire, find out of the user-specfied pin is still active
         if (debounce_clock == 1) begin
             edge_detected <= (button_sync[1] == ACTIVE);
             debounce_clock <= 0;
